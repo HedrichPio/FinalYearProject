6a5e4b6d966333871ce54bb4f386905b40e2af82
HedrichPio <mhpiofernando1@gmail.com>
2023-04-09 01:26:20 +0530

    added get api getResultsAPI with clipping parameters from the front end

--- Backend/hello.py
+++ Backend/hello.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_file, request
 from flask_cors import CORS, cross_origin
+from PIL import Image
 
 app = Flask(__name__)
 
@@ -16,13 +17,37 @@ def get_data():
     return jsonify(data)
 
 
-@app.route('/about', methods=['GET'])
-def get_about():
-    return '<h1>Hello, about!</h1>'
+
+@app.route('/getResultsAPI')
+@cross_origin()
+def get_newdata():
+    # Get the values of the parameters from the request URL
+    x1 = int(request.args.get('x1'))
+    y1 = int(request.args.get('y1'))
+    x2 = int(request.args.get('x2'))
+    y2 = int(request.args.get('y2'))
+
+
+    print("Parameters Received from the frontend",x1,y1,x2,y2)
+
+    # Open the image file
+    results_map = Image.open("assets/result_map.jpg")
+
+    clipped_image = results_map.crop((x1,y1,x2,y2))
+
+    clipped_image.save("assets/clipped.jpg")
+
+    clipped = open("assets/clipped.jpg", "rb")
+
+    # Return the image file as a response
+    return send_file(clipped, mimetype='image/jpeg')
+
+
+
 
 
 if __name__ == '__main__':
-    app.run(port=8070, debug=True)
+    app.run(port=3000, debug=True)
 
 
 
--- Scripts/Testing/xyz_to_tif.py
+++ Scripts/Testing/xyz_to_tif.py
@@ -3,18 +3,25 @@ import os
 
 
 # assign directory
-inputFile = '/Users/hedrichfernando/Downloads/predicted_XYZ/model10_test_2_1_preds_coords.xyz'
-outputFile ='/Users/hedrichfernando/Downloads/test2_jpeg.jpg'
+#inputFile = '/Users/hedrichfernando/Downloads/predicted_XYZ/model10_test_2_1_preds_coords.xyz'
+#outputFile ='/Users/hedrichfernando/Downloads/test2_jpeg.jpg'
      
 #process = "gdal_translate "+inputFile+" "+outputFile
 #os.system(process)
 
 
-process = "gdal_translate -of JPEG -scale -co worldfile=yes "+inputFile+" "+outputFile
-os.system(process)
+#process = "gdal_translate -of JPEG -scale -co worldfile=yes "+inputFile+" "+outputFile
+#os.system(process)
     
-    
-    
+input_a = '/Users/hedrichfernando/Downloads/FYP/RemoteSensingData/Optical_RS/testData/2/test_planet/2_1_xyz/xyz_ndvi_clipped_2022_07_19_harmonized_clip.xyz'
+
+output_a = '/Users/hedrichfernando/Downloads/xyz_to_jpg2.jpg'
+
+
+process_a = "gdal_translate -of JPEG -scale "+input_a+" "+output_a
+
+os.system(process_a)
+
     
     
     
